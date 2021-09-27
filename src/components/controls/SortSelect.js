971f3b2d21e4a315b102376e0d96c4c6a204d419
rajmvjn <rajmvjn@gmail.com>
2021-09-27 11:17:12 +0530

    updated the test cases..

--- src/components/controls/SortSelect.js
+++ src/components/controls/SortSelect.js
@@ -4,8 +4,8 @@ import styles from './SortSelect.module.css';
 const SortSelect = props => {
     return (
         <Fragment>
-            { props.label && <label className={styles.label}> {props.label} </label> }
-            <select type="text" onChange={props.changeHandler}>
+            { props.label && <label className={styles.label} htmlFor='select-label' > {props.label} </label> }
+            <select type="text" onChange={props.changeHandler} id='select-label'>
                 {props.options.map((option, index) => <option key={index}>{option}</option>)}
             </select>
         </Fragment>
--- src/tests/components/controls/SortSelect.test.js
+++ src/tests/components/controls/SortSelect.test.js
@@ -4,15 +4,21 @@ import SortSelect from '../../../components/controls/SortSelect';
 describe('Sort Select component', () => {
     test('renders the given label for the select element', () => {
 
-        render(<SortSelect label={'Select Label'} options={[]}/>);
-        const labelElement = screen.getByText('Select Label');
+        let labelText = 'Select Label';
+        
+        render(<SortSelect label={labelText} options={[]}/>);
+        const labelElement = screen.getByLabelText(labelText);
+        screen.debug(labelElement);
+        screen.logTestingPlaygroundURL(labelElement);
         expect(labelElement).toBeInTheDocument();
     });
 
     test('renders the select option with given options', () => {
 
-        render(<SortSelect options={['option1', 'option2']}/>);
-        const labelElement = screen.getByText('option2');
+        let option2 = 'option2';
+
+        render(<SortSelect options={['option1', option2 ]}/>);
+        const labelElement = screen.getByText(option2);
         expect(labelElement).toBeInTheDocument();
     });
 });
